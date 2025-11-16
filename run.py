4de764a3461ee4d705c2116d8c7ce7120f2c8cd6
Apoorv Rathore <0211ite094@niet.co.in>
2025-11-16 21:52:49 +0530

    update url address

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app, db
 from sqlalchemy import text
 from flask_socketio import SocketIO
+import eventlet
+import eventlet.wsgi
 
 app = create_app()
 socket_io = SocketIO(app,cors_allowed_origins="*")
@@ -19,3 +22,6 @@ with app.app_context():
 if __name__ == "__main__":
     # app.run(host="127.0.0.1", port=5000, debug=True)
     socket_io.run(app,host="127.0.0.1",port=5000,debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    eventlet.wsgi.server(eventlet.listen(("0.0.0.0", port)), app)
+
